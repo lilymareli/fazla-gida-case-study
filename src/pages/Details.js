be662dcbfdae3e5d981f27ffacab4a80de9f32a1
lilymareli <mervekucukofficial@gmail.com>
2020-07-30 16:31:41 +0300

    refresh control & props to Details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,11 +1,17 @@
 import React, {useEffect, useState} from 'react'
-import {SafeAreaView, View, Text} from 'react-native'
+import {SafeAreaView, View, Text, Button, Linking} from 'react-native'
 
 const Details = (props) => {
+    const facebook = `https://www.facebook.com/search/top/?q=${props.route.params.name}%20${props.route.params.surname}`
     return(
     <SafeAreaView>
         <View>
             <Text>Details</Text>
+            <Text> {props.route.params.name} </Text>
+        <Button title="facebook" onPress={() => Linking.openURL(facebook) } />
+
+        
+
         </View>
     </SafeAreaView>
     )
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { SafeAreaView, View, Text, Button, FlatList, Alert, Image } from 'react-native'
+import { SafeAreaView, View, Text, Button, FlatList, Alert, Image, Pressable } from 'react-native'
 
 const Main = (props) => {
 
@@ -11,17 +11,18 @@ const Main = (props) => {
         fetchData()
     }, [])
 
+    //fetchData fonksiyonunu direkt useEffect'e yazamıyorum çünkü asenkron fonksiyon yazdırmıyor.
+    //O sebeple ayrı bir fonksiyon yazıp useEffect ile çağırdım.
     const fetchData = async () => {
         setLoading(true)
         axios.get("https://randomuser.me/api/?results=10")
             .then(response => {
                 setUserList(response.data.results)
                 setLoading(false)
-
             })
             .catch(error => {
                 setLoading(false)
-                Alert.alert("My App", "Bir hata oluştu!")
+                Alert.alert("Fazla Gıda", "Bir hata oluştu!")
             })
 
     }
@@ -29,21 +30,31 @@ const Main = (props) => {
     const renderItem = ({ item }) => {
 
         return (
-            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-<View style={{flexDirection: 'row'}}>
-                <Image source={{ uri: `${item.picture.thumbnail}` }} style={{ width: 50, height: 50, borderRadius: 50 }} />
-               <View>
-
-               <View style={{flexDirection: 'row'}}>
-                <Text>{item.name.first} </Text>
-                <Text>{item.name.last} </Text>
-               </View>
-                <Text> {item.email} </Text>
-               </View>
-</View>
-
-                <Text> {item.gender} </Text>
-            </View>
+            <Pressable onPress={() => props.navigation.navigate("Details", 
+            { 
+                name: item.name.first,
+                surname: item.name.last,
+                mail: item.email,
+                picture: item.picture.medium,
+                mobile: item.cell,
+                phone: item.phone,
+                gender: item.gender,
+                })}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Image source={{ uri: `${item.picture.thumbnail}` }}
+                            style={{ width: 50, height: 50, borderRadius: 50 }} />
+                        <View>
+                            <View style={{ flexDirection: 'row' }}>
+                                <Text>{item.name.first} </Text>
+                                <Text>{item.name.last} </Text>
+                            </View>
+                            <Text> {item.email} </Text>
+                        </View>
+                    </View>
+                    <Text> {item.gender} </Text>
+                </View>
+            </Pressable>
         )
     }
 
@@ -51,13 +62,15 @@ const Main = (props) => {
         <SafeAreaView>
             <View>
                 <Text>Random Users</Text>
-                <Button title="Detay" onPress={() => props.navigation.navigate("Details")} />
                 <FlatList
-                    loading={loading}
                     data={userList}
                     renderItem={renderItem}
+                    //index'i string'e çeviriyoruz çünkü flatlist key olarak yalnızca string kabul ediyor.
                     keyExtractor={(item, index) => index.toString()}
+                    refreshing={loading}
+                    onRefresh={fetchData}
                 />
+                <Text>Scroll Down to Refresh!</Text>
             </View>
         </SafeAreaView>
     )
